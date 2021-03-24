815b0e701fd28bf64ca1e9fe37da89ef14561368
Peter Zettl <mpzettl@gmail.com>
2021-03-24 02:50:35 +0000

    Message Class completed and all tests pass

--- message.js
+++ message.js
@@ -1,5 +1,16 @@
 class Message {
    // Write code here!
+   constructor(name, commands){
+     this.name = 'New message!';
+     if (!name) {
+       throw Error('Message name required.');
+     }
+     this.commands = ('Another message!', commands);
+     
+   }
 }
 
 module.exports = Message;
+//let commands = [new Command('STATUS_CHECK'), new Command('MOVE', 20)];
+ //let message = new Message('Another message!', commands);
+//expect(message.commands).toEqual(commands);
