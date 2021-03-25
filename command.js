6ff66b61dd5b19debedfee513c06c4325bf308e8
Peter Zettl <mpzettl@gmail.com>
2021-03-25 15:24:46 +0000

    Class command filled out with each of the commands. not testing well

--- command.js
+++ command.js
@@ -2,26 +2,32 @@
 class Command {
    constructor(commandType, value) {
      
-     this.roverStatus = ('STATUS_CHECK');
+     this.commandType = ('STATUS_CHECK');
      if (!commandType) {
        throw Error("Command type required.");
      }
-     
-     this.roverPosition = rover.position;//('MOVE', 20);
-
-     this.moveCommand =('MOVE')
-     if (!commandType) {
-       throw Error("Command type required.");
-     }
-     this.movement = (new Rover (rover.position)) + message.commands[1];
+     this.value = ('MOVE', 20);
    
-      this.modeChange=('MODE_CHANGE');
-      if (!commandType) {
-       throw Error("Command type required.");
-     }
-      this.newMode = rover.mode
-   }
- };
+      this.roverStatus = ('STATUS_CHECK');
+        if (!commandType) {
+          throw Error("Command type required.");
+        }
+        
+        this.roverPosition = rover.position;
+
+        this.moveCommand =('MOVE')
+        if (!commandType) {
+          throw Error("Command type required.");
+        }
+        this.movement = (new Rover (rover.position)) + message.commands[1];
+      
+          this.modeChange=('MODE_CHANGE');
+          if (!commandType) {
+          throw Error("Command type required.");
+        }
+          this.newMode = rover.mode
+      }
+ }
  
  module.exports = Command;
 
