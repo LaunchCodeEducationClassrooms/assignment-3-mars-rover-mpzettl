ddd91fccb6f609c076079e84ccc68799cccb1a84
Peter Zettl <mpzettl@gmail.com>
2021-03-29 18:03:56 +0000

    for loop in receiveMessage works

--- rover.js
+++ rover.js
@@ -1,22 +1,28 @@
 class Rover {
   constructor(position,mode,generatorWatts){
     
-    this.position = 87382098;
+    this.position = position;
     this.mode = 'NORMAL';
     this.generatorWatts = 110;
     this.roverStatus ={mode,generatorWatts, position};
+    console.log(8, this.roverStatus);
   }
    // Write code here!
+  
   receiveMessage(message){
-    this.message = [message.name, message.commands];
-    if (message.commands === 'STATUS_CHECK'){
-      this.results = [message.commands], roverStatus;
+    this.message = [message.name];
+    
+    let results = [message.commands];
+    let command;
+    for (command in message.commands){
+    if (command === 'STATUS_CHECK'){
+      this.results = [this.rover.roverStatus];
     }else{
       this.results = [this.message.commands];
     }
-    console.log(7,this.message);
-    return(message);
-   }
+    console.log(7,this.message, this.results);
+    return(message, results);
+   }}
 };
 
 module.exports = Rover;
